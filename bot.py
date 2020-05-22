aeec81980c70056e62c65c9c140ee03f4a87eb9b
kmw070806 <63104149+kmw070806@users.noreply.github.com>
2020-05-22 19:43:30 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -251,6 +251,69 @@ async def on_message(message): # on_message() event : when the bot has recieved 
                 embed.add_field(name="???", value="올바르지 않은 소환사 이름입니다. 다시 확인해주세요!", inline=False)
                 await message.channel.send("Wrong Summoner Nickname", embed=embed)   
 
-                
+
+
+
+
+
+
+
+
+
+
+
+
+
+if message.content.startswith("!코로나"):
+        # 보건복지부 코로나 바이러스 정보사이트"
+        covidSite = "http://ncov.mohw.go.kr/index.jsp"
+        covidNotice = "http://ncov.mohw.go.kr"
+        html = urlopen(covidSite)
+        bs = BeautifulSoup(html, 'html.parser')
+        latestupdateTime = bs.find('span', {'class': "livedate"}).text.split(',')[0][1:].split('.')
+        statisticalNumbers = bs.findAll('span', {'class': 'num'})
+        beforedayNumbers = bs.findAll('span', {'class': 'before'})
+
+        #주요 브리핑 및 뉴스링크
+        briefTasks = []
+        mainbrief = bs.findAll('a',{'href' : re.compile('\/tcmBoardView\.do\?contSeq=[0-9]*')})
+        
+        for brf in mainbrief:
+            container = []
+            container.append(brf.text)
+            container.append(covidNotice + brf['href'])
+            briefTasks.append(container)
+        print(briefTasks)
+
+        # 통계수치
+        statNum = []
+        # 전일대비 수치
+        beforeNum = []
+        for num in range(7):
+            statNum.append(statisticalNumbers[num].text)
+        for num in range(4):
+            beforeNum.append(beforedayNumbers[num].text.split('(')[-1].split(')')[0])
+
+        totalPeopletoInt = statNum[0].split(')')[-1].split(',')
+        tpInt = ''.join(totalPeopletoInt)
+        lethatRate = round((int(statNum[3]) / int(tpInt)) * 100, 2)
+        embed = discord.Embed(title="Covid-19 Virus Korea Status", description="",color=0x5CD1E5)
+        embed.add_field(name="Data source : Ministry of Health and Welfare of Korea", value="http://ncov.mohw.go.kr/index.jsp", inline=False)
+        embed.add_field(name="Latest data refred time",value="해당 자료는 " + latestupdateTime[0] + "월 " + latestupdateTime[1] + "일 "+latestupdateTime[2] +" 자료입니다.", inline=False)
+        embed.add_field(name="확진환자(누적)", value=statNum[0].split(')')[-1]+"("+beforeNum[0]+")",inline=True)
+        embed.add_field(name="완치환자(격리해제)", value=statNum[1] + "(" + beforeNum[1] + ")", inline=True)
+        embed.add_field(name="치료중(격리 중)", value=statNum[2] + "(" + beforeNum[2] + ")", inline=True)
+        embed.add_field(name="사망", value=statNum[3] + "(" + beforeNum[3] + ")", inline=True)
+        embed.add_field(name="누적확진률", value=statNum[6], inline=True)
+        embed.add_field(name="치사율", value=str(lethatRate) + " %",inline=True)
+        embed.add_field(name="- 최신 브리핑 1 : " + briefTasks[0][0],value="Link : " + briefTasks[0][1],inline=False)
+        embed.add_field(name="- 최신 브리핑 2 : " + briefTasks[1][0], value="Link : " + briefTasks[1][1], inline=False)
+        embed.set_thumbnail(url="https://wikis.krsocsci.org/images/7/79/%EB%8C%80%ED%95%9C%EC%99%95%EA%B5%AD_%ED%83%9C%EA%B7%B9%EA%B8%B0.jpg")
+        embed.set_footer(text='Service provided by Hoplin.',
+                         icon_url='https://avatars2.githubusercontent.com/u/45956041?s=460&u=1caf3b112111cbd9849a2b95a88c3a8f3a15ecfa&v=4')
+        await message.channel.send("Covid-19 Virus Korea Status", embed=embed)
+
+
+
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
