4bf81dfd7d2d3cca238c18665e3a22863411cbf4
kmw070806 <63104149+kmw070806@users.noreply.github.com>
2020-07-24 17:22:41 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+# To make a discord bot you need to download discord.py with pip
+#-*- coding: utf-8 -*-
 import discord
 import asyncio
 import os
@@ -19,7 +21,7 @@ tierScore = {
     'bronze' : 2,
     'silver' : 3,
     'gold' : 4,
-    'platinum' : 5,
+    'Platinum' : 5,
     'diamond' : 6,
     'master' : 7,
     'grandmaster' : 8,
@@ -39,7 +41,6 @@ opggsummonersearch = 'https://www.op.gg/summoner/userName='
 bottoken = ''
 
 '''
-Simple Introduction about asyncio
 asyncio : Asynchronous I/O. It is a module for asynchronous programming and allows CPU operations to be handled in parallel with I/O.
 async def (func name)(parameters): -> This type of asynchronous function or method is called Native Co-Rutine.
 - await : you can use await keyword only in Native Co-Rutine
@@ -62,6 +63,8 @@ def deleteTags(htmls):
         htmls[a] = re.sub('<.+?>','',str(htmls[a]),0).strip()
     return htmls
 
+bottoken = ''
+
 @client.event # Use these decorator to register an event.
 async def on_ready(): # on_ready() event : when the bot has finised logging in and setting things up
     await client.change_presence(status=discord.Status.online, activity=discord.Game("Type !help or !도움말 for help"))
@@ -80,44 +83,40 @@ async def on_message(message): # on_message() event : when the bot has recieved 
     if message.author == client.user:
         return
 
-    if message.content.startswith("!help") or message.content.startswith("!도움말"):
-        embed = discord.Embed(title="명령어 사용방법!", description="!전적 (소환사 이름 - 띄어쓰기 붙여쓰기 상관없습니다)", color=0x5CD1E5)
-        embed.set_footer(text='Service provided by Hoplin.',
-                         icon_url='https://avatars2.githubusercontent.com/u/45956041?s=460&u=1caf3b112111cbd9849a2b95a88c3a8f3a15ecfa&v=4')
-        await message.channel.send("도움말!", embed=embed)
-
     if message.content.startswith("!롤전적"):
-        try:
-            if len(message.content.split(" ")) == 1:
-                embed = discord.Embed(title="소환사 이름이 입력되지 않았습니다!", description="", color=0x5CD1E5)
-                embed.add_field(name="Summoner name not entered",
-                                value="To use command !롤전적 : !롤전적 (Summoner Nickname)", inline=False)
-                embed.set_footer(text='Service provided by Hoplin.',
-                                 icon_url='https://avatars2.githubusercontent.com/u/45956041?s=460&u=1caf3b112111cbd9849a2b95a88c3a8f3a15ecfa&v=4')
-                await message.channel.send("Error : Incorrect command usage ", embed=embed)
-            else:
-                playerNickname = ''.join((message.content).split(' ')[1:])
-                # Open URL
-                checkURLBool = urlopen(opggsummonersearch + quote(playerNickname))
-                bs = BeautifulSoup(checkURLBool, 'html.parser')
+        playerNickname = ''.join((message.content).split(' ')[1:])
+        # Open URL
+        checkURLBool = urlopen(opggsummonersearch + quote(playerNickname))
+        bs = BeautifulSoup(checkURLBool, 'html.parser')
 
-                # 자유랭크 언랭은 뒤에 '?image=q_auto&v=1'표현이없다
+        # 자유랭크 언랭은 뒤에 '?image=q_auto&v=1'표현이없다
+        RankMedal = bs.findAll('img', {
+        'src': re.compile('\/\/[a-z]*\-[A-Za-z]*\.[A-Za-z]*\.[A-Za-z]*\/[A-Za-z]*\/[A-Za-z]*\/[a-z0-9_]*\.png')})
+        # index 0 : Solo Rank
+        # index 1 : Flexible 5v5 rank
 
-                # Patch Note 20200503에서
-                # Medal = bs.find('div', {'class': 'ContentWrap tabItems'}) 이렇게 바꾸었었습니다.
-                # PC의 설정된 환경 혹은 OS플랫폼에 따라서 ContentWrap tabItems의 띄어쓰기가 인식이
+        # for mostUsedChampion
+        mostUsedChampion = bs.find('div', {'class': 'ChampionName'})
+        mostUsedChampionKDA = bs.find('span', {'class': 'KDA'})
 
-                Medal = bs.find('div', {'class': 'SideContent'})
-                RankMedal = Medal.findAll('img', {'src': re.compile('\/\/[a-z]*\-[A-Za-z]*\.[A-Za-z]*\.[A-Za-z]*\/[A-Za-z]*\/[A-Za-z]*\/[a-z0-9_]*\.png')})
-                # Variable RankMedal's index 0 : Solo Rank
-                # Variable RankMedal's index 1 : Flexible 5v5 rank
+        # 솔랭, 자랭 둘다 배치가 안되어있는경우 -> 사용된 챔피언 자체가 없다. 즉 모스트 챔피언 메뉴를 넣을 필요가 없다.
 
-                # for mostUsedChampion
-                mostUsedChampion = bs.find('div', {'class': 'ChampionName'})
-                mostUsedChampionKDA = bs.find('span', {'class': 'KDA'})
+        if len(message.content.split(" ")) == 1:
+            embed = discord.Embed(title="소환사 이름이 입력되지 않았습니다!", description="", color=0x5CD1E5)
+            embed.add_field(name="해당 닉네임의 소환사가 존재하지 않습니다",
+                            value="To use command !롤전적 : !롤전적 (Summoner Nickname)", inline=False)
+            embed.set_footer(text='Service provided by sketa.',
+                             icon_url='https://cdn.discordapp.com/attachments/702316283659550740/706160324260266095/2424b25e9404216025533fc240e33acd.jpg')
+            await message.channel.send("Error : Incorrect command usage ", embed=embed)
 
-                # 솔랭, 자랭 둘다 배치가 안되어있는경우 -> 사용된 챔피언 자체가 없다. 즉 모스트 챔피언 메뉴를 넣을 필요가 없다.
-
+        elif len(deleteTags(bs.findAll('h2', {'class': 'Title'}))) != 0:
+            embed = discord.Embed(title="존재하지 않는 소환사", description="", color=0x5CD1E5)
+            embed.add_field(name="해당 닉네임의 소환사가 존재하지 않습니다.", value="소환사 이름을 확인해주세요", inline=False)
+            embed.set_footer(text='Service provided by sketa.',
+                             icon_url='https://cdn.discordapp.com/attachments/702316283659550740/706160324260266095/2424b25e9404216025533fc240e33acd.jpg')
+            await message.channel.send(" ", embed=embed)
+        else:
+            try:
                 # Scrape Summoner's Rank information
                 # [Solorank,Solorank Tier]
                 solorank_Types_and_Tier_Info = deleteTags(bs.findAll('div', {'class': {'RankType', 'TierRank'}}))
@@ -136,14 +135,14 @@ async def on_message(message): # on_message() event : when the bot has recieved 
                 # 솔랭, 자랭 둘다 배치 안되어있는 경우 -> 모스트 챔피언 출력 X
                 if len(solorank_Point_and_winratio) == 0 and len(flexrank_Point_and_winratio) == 0:
                     embed = discord.Embed(title="소환사 전적검색", description="", color=0x5CD1E5)
-                    embed.add_field(name="Summoner Search From op.gg", value=opggsummonersearch + playerNickname,
+                    embed.add_field(name="해당 전적은 op.gg 에서 가져왔습니다", value=opggsummonersearch + playerNickname,
                                     inline=False)
-                    embed.add_field(name="Ranked Solo : Unranked", value="Unranked", inline=False)
-                    embed.add_field(name="Flex 5:5 Rank : Unranked", value="Unranked", inline=False)
+                    embed.add_field(name="솔로랭크 : Unranked", value="Unranked", inline=False)
+                    embed.add_field(name="자유랭크 : Unranked", value="Unranked", inline=False)
                     embed.set_thumbnail(url='https:' + RankMedal[0]['src'])
-                    embed.set_footer(text='Service provided by Hoplin.',
-                                     icon_url='https://avatars2.githubusercontent.com/u/45956041?s=460&u=1caf3b112111cbd9849a2b95a88c3a8f3a15ecfa&v=4')
-                    await message.channel.send("소환사 " + playerNickname + "님의 전적", embed=embed)
+                    embed.set_footer(text='Service provided by sketa.',
+                                     icon_url='https://cdn.discordapp.com/attachments/702316283659550740/706160324260266095/2424b25e9404216025533fc240e33acd.jpg')
+                    await message.channel.send(" ", embed=embed)
 
                 # 솔로랭크 기록이 없는경우
                 elif len(solorank_Point_and_winratio) == 0:
@@ -156,20 +155,20 @@ async def on_message(message): # on_message() event : when the bot has recieved 
                     mostUsedChampionWinRate = bs.find('div', {'class': "Played"})
                     mostUsedChampionWinRate = mostUsedChampionWinRate.div.text.strip()
 
-                    FlexRankTier = flexrank_Types_and_Tier_Info[0] + ' : ' + flexrank_Types_and_Tier_Info[1]
+                    FlexRankTier = ' : ' + flexrank_Types_and_Tier_Info[1]
                     FlexRankPointAndWinRatio = flexrank_Types_and_Tier_Info[2] + " /" + flexrank_Types_and_Tier_Info[-1]
                     embed = discord.Embed(title="소환사 전적검색", description="", color=0x5CD1E5)
-                    embed.add_field(name="Summoner Search From op.gg", value=opggsummonersearch + playerNickname,
+                    embed.add_field(name="해당 전적은 op.gg 에서 가져왔습니다.", value=opggsummonersearch + playerNickname,
                                     inline=False)
-                    embed.add_field(name="Ranked Solo : Unranked", value="Unranked", inline=False)
-                    embed.add_field(name=FlexRankTier, value=FlexRankPointAndWinRatio, inline=False)
-                    embed.add_field(name="Most Used Champion : " + mostUsedChampion,
-                                    value="KDA : " + mostUsedChampionKDA + " / " + " WinRate : " + mostUsedChampionWinRate,
+                    embed.add_field(name="솔로랭크 : Unranked", value="Unranked", inline=False)
+                    embed.add_field(name="자유랭크" + FlexRankTier, value=FlexRankPointAndWinRatio, inline=False)
+                    embed.add_field(name="자주 사용하는 챔피언 : " + mostUsedChampion,
+                                    value="KDA : " + mostUsedChampionKDA + " / " + " winrate : " + mostUsedChampionWinRate,
                                     inline=False)
                     embed.set_thumbnail(url='https:' + RankMedal[1]['src'])
-                    embed.set_footer(text='Service provided by Hoplin.',
-                                     icon_url='https://avatars2.githubusercontent.com/u/45956041?s=460&u=1caf3b112111cbd9849a2b95a88c3a8f3a15ecfa&v=4')
-                    await message.channel.send("소환사 " + playerNickname + "님의 전적", embed=embed)
+                    embed.set_footer(text='Service provided by sketa.',
+                                     icon_url='https://cdn.discordapp.com/attachments/702316283659550740/706160324260266095/2424b25e9404216025533fc240e33acd.jpg')
+                    await message.channel.send(" ", embed=embed)
 
                 # 자유랭크 기록이 없는경우
                 elif len(flexrank_Point_and_winratio) == 0:
@@ -182,21 +181,21 @@ async def on_message(message): # on_message() event : when the bot has recieved 
                     mostUsedChampionWinRate = bs.find('div', {'class': "Played"})
                     mostUsedChampionWinRate = mostUsedChampionWinRate.div.text.strip()
 
-                    SoloRankTier = solorank_Types_and_Tier_Info[0] + ' : ' + solorank_Types_and_Tier_Info[1]
+                    SoloRankTier = " : " + solorank_Types_and_Tier_Info[1]
                     SoloRankPointAndWinRatio = solorank_Point_and_winratio[0] + "/ " + solorank_Point_and_winratio[
                         1] + " " + solorank_Point_and_winratio[2] + " /" + solorank_Point_and_winratio[3]
                     embed = discord.Embed(title="소환사 전적검색", description="", color=0x5CD1E5)
-                    embed.add_field(name="Summoner Search From op.gg", value=opggsummonersearch + playerNickname,
+                    embed.add_field(name="해당 전적은 op.gg 에서 가져왔습니다", value=opggsummonersearch + playerNickname,
                                     inline=False)
-                    embed.add_field(name=SoloRankTier, value=SoloRankPointAndWinRatio, inline=False)
-                    embed.add_field(name="Flex 5:5 Rank : Unranked", value="Unranked", inline=False)
-                    embed.add_field(name="Most Used Champion : " + mostUsedChampion,
-                                    value="KDA : " + mostUsedChampionKDA + " / " + "WinRate : " + mostUsedChampionWinRate,
+                    embed.add_field(name="솔로랭크" +SoloRankTier, value=SoloRankPointAndWinRatio, inline=False)
+                    embed.add_field(name="자유랭크 : Unranked", value="Unranked", inline=False)
+                    embed.add_field(name="자주 사용하는 챔피언 : " + mostUsedChampion,
+                                    value="KDA : " + mostUsedChampionKDA + " / " + "winrate : " + mostUsedChampionWinRate,
                                     inline=False)
                     embed.set_thumbnail(url='https:' + RankMedal[0]['src'])
-                    embed.set_footer(text='Service provided by Hoplin.',
-                                     icon_url='https://avatars2.githubusercontent.com/u/45956041?s=460&u=1caf3b112111cbd9849a2b95a88c3a8f3a15ecfa&v=4')
-                    await message.channel.send("소환사 " + playerNickname + "님의 전적", embed=embed)
+                    embed.set_footer(text='Service provided by sketa.',
+                                     icon_url='https://cdn.discordapp.com/attachments/702316283659550740/706160324260266095/2424b25e9404216025533fc240e33acd.jpg')
+                    await message.channel.send(" ", embed=embed)
                 # 두가지 유형의 랭크 모두 완료된사람
                 else:
                     # 더 높은 티어를 thumbnail에 안착
@@ -204,10 +203,10 @@ async def on_message(message): # on_message() event : when the bot has recieved 
                     flexrankmedal = RankMedal[1]['src'].split('/')[-1].split('?')[0].split('.')[0].split('_')
 
                     # Make State
-                    SoloRankTier = solorank_Types_and_Tier_Info[0] + ' : ' + solorank_Types_and_Tier_Info[1]
+                    SoloRankTier = ' : ' + solorank_Types_and_Tier_Info[1]
                     SoloRankPointAndWinRatio = solorank_Point_and_winratio[0] + "/ " + solorank_Point_and_winratio[
                         1] + " " + solorank_Point_and_winratio[2] + " /" + solorank_Point_and_winratio[3]
-                    FlexRankTier = flexrank_Types_and_Tier_Info[0] + ' : ' + flexrank_Types_and_Tier_Info[1]
+                    FlexRankTier = ' : ' + flexrank_Types_and_Tier_Info[1]
                     FlexRankPointAndWinRatio = flexrank_Types_and_Tier_Info[2] + " /" + flexrank_Types_and_Tier_Info[-1]
 
                     # most Used Champion Information : Champion Name, KDA, Win Rate
@@ -220,12 +219,12 @@ async def on_message(message): # on_message() event : when the bot has recieved 
 
                     cmpTier = tierCompare(solorankmedal[0], flexrankmedal[0])
                     embed = discord.Embed(title="소환사 전적검색", description="", color=0x5CD1E5)
-                    embed.add_field(name="Summoner Search From op.gg", value=opggsummonersearch + playerNickname,
+                    embed.add_field(name="해당 전적은 op.gg 에서 가져왔습니다", value=opggsummonersearch + playerNickname,
                                     inline=False)
-                    embed.add_field(name=SoloRankTier, value=SoloRankPointAndWinRatio, inline=False)
-                    embed.add_field(name=FlexRankTier, value=FlexRankPointAndWinRatio, inline=False)
-                    embed.add_field(name="Most Used Champion : " + mostUsedChampion,
-                                    value="KDA : " + mostUsedChampionKDA + " / " + " WinRate : " + mostUsedChampionWinRate,
+                    embed.add_field(name='솔로랭크 : ' + SoloRankTier, value=SoloRankPointAndWinRatio, inline=False)
+                    embed.add_field(name='자유랭크 : ' + FlexRankTier, value=FlexRankPointAndWinRatio, inline=False)
+                    embed.add_field(name="자주 사용하는 챔피언 : " + mostUsedChampion,
+                                    value="KDA : " + mostUsedChampionKDA + " / " + " winrate : " + mostUsedChampionWinRate,
                                     inline=False)
                     if cmpTier == 0:
                         embed.set_thumbnail(url='https:' + RankMedal[0]['src'])
@@ -239,26 +238,19 @@ async def on_message(message): # on_message() event : when the bot has recieved 
                         else:
                             embed.set_thumbnail(url='https:' + RankMedal[0]['src'])
 
-                    embed.set_footer(text='Service provided by Hoplin.',
-                                     icon_url='https://avatars2.githubusercontent.com/u/45956041?s=460&u=1caf3b112111cbd9849a2b95a88c3a8f3a15ecfa&v=4')
-                    await message.channel.send("소환사 " + playerNickname + "님의 전적", embed=embed)
-        except HTTPError as e:
-            embed = discord.Embed(title="소환사 전적검색 실패", description="", color=0x5CD1E5)
-            embed.add_field(name="", value="올바르지 않은 소환사 이름입니다. 다시 확인해주세요!", inline=False)
-            await message.channel.send("Wrong Summoner Nickname")
+                    embed.set_footer(text='Service provided by sketa.',
+                                     icon_url='https://cdn.discordapp.com/attachments/702316283659550740/706160324260266095/2424b25e9404216025533fc240e33acd.jpg')
+                    await message.channel.send(" ", embed=embed)
+            except HTTPError as e:
+                embed = discord.Embed(title="소환사 전적검색 실패", description="", color=0x5CD1E5)
+                embed.add_field(name="", value="올바르지 않은 소환사 이름입니다. 다시 확인해주세요!", inline=False)
+                await message.channel.send("Wrong Summoner Nickname")
 
-        except UnicodeEncodeError as e:
-            embed = discord.Embed(title="소환사 전적검색 실패", description="", color=0x5CD1E5)
-            embed.add_field(name="???", value="올바르지 않은 소환사 이름입니다. 다시 확인해주세요!", inline=False)
-            await message.channel.send("Wrong Summoner Nickname", embed=embed)
+            except UnicodeEncodeError as e:
+                embed = discord.Embed(title="소환사 전적검색 실패", description="", color=0x5CD1E5)
+                embed.add_field(name="???", value="올바르지 않은 소환사 이름입니다. 다시 확인해주세요!", inline=False)
+                await message.channel.send("Wrong Summoner Nickname", embed=embed)   
 
-        except AttributeError as e:
-            embed = discord.Embed(title="존재하지 않는 소환사", description="", color=0x5CD1E5)
-            embed.add_field(name="해당 닉네임의 소환사가 존재하지 않습니다.", value="소환사 이름을 확인해주세요", inline=False)
-            embed.set_footer(text='Service provided by Hoplin.',
-                             icon_url='https://avatars2.githubusercontent.com/u/45956041?s=460&u=1caf3b112111cbd9849a2b95a88c3a8f3a15ecfa&v=4')
-            await message.channel.send("Error : Non existing Summoner ", embed=embed)
-                       
-        
+                
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
